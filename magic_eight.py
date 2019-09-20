1faa1223d8dc4eb7c1cab7f50cb337e8c27eddd8
Cheng Zhou <zhoucheng95@hotmail.com>
2019-09-20 17:24:31 -0400

    add questions from C

--- magic_eight.py
+++ magic_eight.py
@@ -1,3 +1,14 @@
+import random
+
+
 def ask_questions():
     x=input("What is your question?")
     return x
+
+
+def add_questions():
+    num = random.randint(0, 9)
+    answers = ["Reply hazy,try again.", "Ask again later.", "Better not tell you now", "Cannot predict now.",
+               "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.",
+               "Outlook not so good.", "Very doubtful."]
+    print(answers[num])
