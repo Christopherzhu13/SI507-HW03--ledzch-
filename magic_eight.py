288b7c16b80f69a389d8d634966de8012aeb7e56
Chenhan Zhu <ledzch@umich.edu>
2019-09-20 17:27:47 -0400

    check

--- magic_eight.py
+++ magic_eight.py
@@ -1,3 +1,10 @@
 def ask_questions():
     x=input("What is your question?")
     return x
+while (True):
+    x=ask_questions()
+    if x=="quit":
+        break
+    if x[-1]!='?':
+        print("I’m sorry, I can only answer questions.")
+        continue
