e26d2021ad10ad2116ba994c911c2e140bf39733
Cheng Zhou <zhoucheng95@hotmail.com>
2019-09-20 17:46:14 -0400

    C1

--- magic_eight.py
+++ magic_eight.py
@@ -9,3 +9,10 @@ def pick_answer():
             ,"yes - definitely.","You may rely on it.","Most likely","Outlook good.","Yes","Signs point to yes."
            ]
     print(answer[a])
+
+def add_questions():
+    num = random.randint(0, 9)
+    answers = ["Reply hazy,try again.", "Ask again later.", "Better not tell you now", "Cannot predict now.",
+               "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.",
+               "Outlook not so good.", "Very doubtful."]
+    print(answers[num])
