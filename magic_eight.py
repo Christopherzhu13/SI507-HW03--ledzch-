0a0fd8df7ac12e124058f0fac394d82c1c0853d2
Chenhan Zhu <ledzch@umich.edu>
2019-09-20 18:02:58 -0400

    final

--- magic_eight.py
+++ magic_eight.py
@@ -17,11 +17,3 @@ def add_questions():
                "Outlook not so good.", "Very doubtful."]
     print(answers[num])
 
-
-while (True):
-    x=ask_questions()
-    if x=="quit":
-        break
-    if x[-1]!='?':
-        print("I’m sorry, I can only answer questions.")
-        continue
