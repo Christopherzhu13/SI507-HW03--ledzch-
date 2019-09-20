b87aab51f97e50d2fe8a4e29b29cfbddd85d043a
Wenhao_LIU <wswenhao@umich.edu>
2019-09-20 17:18:43 -0400

    add_questionsB

--- magic_eight.py
+++ magic_eight.py
@@ -1,3 +1,11 @@
+import random
 def ask_questions():
     x=input("What is your question?")
     return x
+
+def pick_answer():
+    a=random.randint(0,9)
+    answer=["It is certain.","As I see it, yes","It is decidedly so.","Without a doubt."
+            ,"yes - definitely.","You may rely on it.","Most likely","Outlook good.","Yes","Signs point to yes."
+           ]
+    print(answer[a])
